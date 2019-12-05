06ea81e529df074dd4509ac8ebd785653c6aff8e
Bhavya Shah <bhavyashah9612@gmail.com>
2019-12-04 20:17:26 -0500

    third view added

---
+++ Todo_List_B/InfoViewController.swift
@@ -0,0 +1,22 @@
+//
+//  InfoViewController.swift
+//  Todo_List_B
+//
+//  Created by Student on 2019-12-04.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UITextField!
+    
+    @IBOutlet weak var descField: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = list[myIndex]
+        descField.text = descList[myIndex]
+    }
+}
